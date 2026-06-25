0e65e2e349bba8deb002e2b7704893671f79b82e
Likitha D <likithad@Likithas-MacBook-Pro.local>
2026-06-24 22:30:07 -0500

    new logic added

--- Password_Checker.py
+++ Password_Checker.py
@@ -2,18 +2,24 @@
 password = input("Enter the password")
 score = 0
 def pass_checker(password,score):
+  forbidden = '^*%_;'
   symbols = '@,#,$'
   if len(password) >=8 :
     score+=20
   if  any (char in symbols for char in password):
-    score+=40
+    score+=20
   if  any(char.isdigit()for char in password):
-    score+=40
-    
+    score+=20
+  if any(char.lower() for char in password):
+    score+=20
+  if any(char.upper() for char in password):
+    score+=20
+  if any(char in forbidden for char in password):
+    score -=20
   return score
   
 
-if score <80:
+if score <=80:
   print(f"your password {pass_checker(password,score)}% only please make is stronger")
 else:
   print(f"Your password {pass_checker(password,score)} is stronger!!")
