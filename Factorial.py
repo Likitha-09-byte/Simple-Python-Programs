f9d32434aa3aa85ab8971d585acd9ff43f7543d3
Likitha D <likithad@Likithas-MacBook-Pro.local>
2026-06-26 11:11:51 -0500

    Factorial function

---
+++ Factorial.py
@@ -0,0 +1,9 @@
+#Check the factorial of a number using recursion:
+n = int(input("enter the number : "))
+def factorial(n):
+  if n ==1 or n==0:
+    return 1
+  else:
+    return n * factorial(n-1)
+
+print(factorial(n))
