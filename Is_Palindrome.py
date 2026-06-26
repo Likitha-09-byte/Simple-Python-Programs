9460ed8417ffa95bc88ba1f6d1d40fff6afe7d96
Likitha D <likithad@Likithas-MacBook-Pro.local>
2026-06-26 10:50:40 -0500

    Palindrome function

---
+++ Is_Palindrome.py
@@ -0,0 +1,13 @@
+#Check whether the given string is palindrome or not .
+targetword = input("Enter the word: ")
+def is_palindrome(targetword):
+  temp =""
+  for i in targetword:
+    temp = i+temp
+  if temp == targetword:
+    return "Palindrome"
+  else:
+    return "Not a Palindrome"
+  
+result =is_palindrome(targetword)
+print(result)
